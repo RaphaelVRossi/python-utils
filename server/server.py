b8ffe42ebe18ec16b543c1833a3dfe46e7044198
ios8043 <ios8043@Mac-mini-3.local>
2019-02-02 15:59:58 -0200

    add post example

--- server/server.py
+++ server/server.py
@@ -1,9 +1,16 @@
 import BaseHTTPServer
+import urlparse
 
 class SimpleRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         print self.path
         self.wfile.write('HTTP/1.0 200 Okay\r\n\r\nHere is your output for '+self.path)
+
+    def do_POST(self):
+	length = int(self.headers.getheader('content-length'))
+        field_data = self.rfile.read(length)
+        fields = urlparse.parse_qs(field_data)
+        print field_data
 
 def run(server_class=BaseHTTPServer.HTTPServer,
     handler_class=SimpleRequestHandler):
